dd568bf578ae7535d3d33f5a58209da48b7750a5
JaysonGiri <sanjaygiriprakash@unomaha.edu>
2025-01-03 11:35:41 -0600

    Learned extend and include

--- BlogJay/urls.py
+++ BlogJay/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name = 'index'),
+    path('', views.index, name = 'index'),
+    path('blogpost/<id>/', views.blogpost, name="blogpost"),
+    path('about/', views.about, name="about"),
 ]
--- BlogJay/views.py
+++ BlogJay/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 
 # Create your views here.
@@ -7,3 +7,16 @@ def index(request):
     return render(request, "BlogJay/index.html", context={
         "posts" : posts
     })
+
+def blogpost(request, id):
+    try:
+        post = get_object_or_404(Post, id=id)
+    except:
+        return redirect("index")
+    
+    return render(request, "BlogJay/blogpost.html", context={
+        "post": post
+    })
+
+def about(request):
+    return render(request, "BlogJay/about.html", context=None)
