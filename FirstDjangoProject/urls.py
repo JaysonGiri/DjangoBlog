435b88320f7f685487f8049c119039a95cda5fab
JaysonGiri <sanjaygiriprakash@unomaha.edu>
2025-01-07 11:35:34 -0600

    display post images

--- FirstDjangoProject/urls.py
+++ FirstDjangoProject/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import settings, static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("BlogJay.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
